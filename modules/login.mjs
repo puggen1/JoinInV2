6ff76ad07fffb4997bb39df309af9d83fe19b4f6
puggen1 <43915072+puggen1@users.noreply.github.com>
2022-10-04 09:19:06 +0200

    moved function. token to localstorage
    login and register is moved to own file.
    auth token and name is added to local storage if login is successfull.

---
+++ modules/login.mjs
@@ -0,0 +1,29 @@
+import { hiddenToggler, displayResponse } from "./responses.mjs";
+//import { eventNames } from "process";
+import globalApiCall from "./globalApiCall.mjs";
+
+/**
+ *
+ * @param {Form Event} event all info needed, given from eventListener
+ */
+export default async function loginUser(event) {
+  event.preventDefault();
+  let { inputEmail = event.target[0], inputPassword = event.target[1] } = event;
+  let response = await globalApiCall("social/auth/login", "", "POST", {
+    email: inputEmail.value,
+    password: inputPassword.value,
+  });
+  if (response.authorizationToken || !response.message) {
+    console.log(response);
+    hiddenToggler(loginResponse);
+    displayResponse(loginResponse, "");
+    localStorage.setItem("token", response.authorizationToken);
+    localStorage.setItem("username", response.name);
+    window.location.href = "./feed.html";
+  } else {
+    let loginResponse = document.querySelector("#loginResponse");
+    hiddenToggler(loginResponse, false);
+    displayResponse(loginResponse, `<p class="m-auto">${response.message}</p>`);
+  }
+  //if response is ok /  token is retrieved, store token and name, redirect to profile or feed
+}
---
+++ modules/register.mjs
@@ -0,0 +1,46 @@
+import globalApiCall from "./globalApiCall.mjs";
+import validate from "./validation.mjs";
+import {
+  hiddenToggler,
+  displayResponse,
+  changeTypeAndColor,
+} from "./responses.mjs";
+
+export default async function registerUser(event) {
+  event.preventDefault();
+  //for success or error message
+  let responseMessage;
+  let registerResponse = document.querySelector("#registerResponse");
+  let validateResult = await validate(event);
+  let { username, email, password } = validateResult;
+  let {
+    inputName = event.target[0],
+    inputEmail = event.target[1],
+    inputPassword = event.target[2],
+  } = event;
+
+  if (username && email && password) {
+    console.log(event);
+    let response = await globalApiCall("social/auth/register", "", "POST", {
+      name: inputName.value,
+      email: inputEmail.value,
+      password: inputPassword.value,
+    });
+    console.log(response);
+    if (!response.email) {
+      changeTypeAndColor(registerResponse, "danger");
+      responseMessage = response.message;
+    } else {
+      changeTypeAndColor(registerResponse, "success");
+      responseMessage = "Account created";
+    }
+    //do something with response
+    // if response is error, display error and change green input fielt to red
+    //else if success, show success message, redirect to profile after one second?
+    displayResponse(
+      registerResponse,
+      `<p class="m-auto">${responseMessage}</p>`
+    );
+    hiddenToggler(registerResponse, false);
+  }
+}

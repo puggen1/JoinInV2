43dd9b030bba39a2c7c88b5178fe553a05e81cff
puggen1 <43915072+puggen1@users.noreply.github.com>
2022-10-06 10:02:56 +0200

    added filler img, post options, coments and methods

--- modules/Post.mjs
+++ modules/Post.mjs
@@ -5,21 +5,57 @@ export default class Post {
   logPost() {
     console.log(this.postData);
   }
+  // if author.name is equal to localstorage name
+  // display edit and delete button
+  /**
+   * @description used to show either only link to single post, or if it is your post, options like delete and update.
+   * @param {string} localUsername username stored in localstorage
+   * @returns string with html of buttons
+   */
+  customButtons(localUsername) {
+    let buttons = `<a type="button" href="./post?id=${this.postData.id}" class="btn btn-outline-dark col-3 m-0 px-0">View </a>`;
+    //quick test this.postData.author.name === localUsername
+    if (this.postData.id === 1458) {
+      let updateBtn = document.createElement("button");
+      updateBtn.setAttribute("data-id", this.postData.id);
+      updateBtn.addEventListener("click", this.updatePost);
+
+      let deleteBtn = document.createElement("button");
+      deleteBtn.setAttribute("data-id", this.postData.id);
+      deleteBtn.addEventListener("click", this.deletePost);
+      buttons += `<div class="dropdown">
+      <button class="btn dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">options</button>
+      <ul class="dropdown-menu">
+          <li><button  data-id="${this.postData.id}" class="dropdown-item">update</button></li>
+          <li><button data-id="${this.postData.id}" class="dropdown-item">delete</button></li>
+          ${updateBtn};
+      </ul>
+  </div>`;
+      console.log(updateBtn);
+    }
+    return buttons;
+  }
+  /**
+   * @description runst trough a post class instance, puts the needed values inside html
+   * @returns html with values from the postData
+   */
   htmlPost() {
+    let user = localStorage.getItem("username");
+    let buttons = this.customButtons(user);
     let picture = Post.postPicture(this.postData);
     let profile = Post.postProfile(this.postData);
     let { title, body, author, created /*updated*/ } = this.postData;
     let html = `<div class="container col-11 col-xl-10 gy-3">
-    <div class="card">
+    <div class="card px-xl-4">
                 <div class="card-body">
                 ${profile}
-                <h3>${title}</h3>
+                <h3 class="">${title}</h3>
                   ${picture}
-                    <p class="card-text px-xl-4">
+                    <p class="card-text ">
                     ${body}
                        </p>
-                    <div class="d-flex mt-3 justify-content-between px-xl-4">
-                    <button type="button" class="btn btn-outline-dark col-3 m-0 px-0">View </button>
+                    <div class="d-flex mt-3 justify-content-between ">
+                    ${buttons}
                     <div class="d-flex justify-content-end">
                         <p class="m-2">${created}</p>
                         <!--<p class="m-2">25 aug</p>-->
@@ -30,20 +66,37 @@ export default class Post {
 </div>`;
     return html;
   }
-  static postPicture(post){
+  /**
+   * @description checks a post object for an picture, and show it in post if so
+   * @param {object} post a single post
+   * @returns string variable with html
+   */
+  static postPicture(post) {
     let img = "";
-    
-    if(post.media){
-      img = `<img src="${post.media} alt="test alt">"`;
+
+    if (post.media) {
+      img = `<img src="${post.media}" class="img-fluid " alt="test alt">`;
     }
     return img;
   }
-  static postProfile(post){
-    let avatar = "";
-    if(post.author.avatar){
-      avatar = `<img src="${post.author.avatar}" class="border-rounded" alt="user test alt">`;
+  /**
+   * @description creates the profile div element for an post
+   * @param {object} post a single post
+   * @returns
+   */
+  static postProfile(post) {
+    let img = "../assets/charlesdeluvio-K4mSJ7kc0As-unsplash.jpg";
+    if (post.author.avatar) {
+      img = post.author.avatar;
     }
-    let userDiv = `<div>${avatar} <p><b class="me-1">${post.author.name}</b>Says:</p></div>`;
-    return userDiv
+    let avatar = `<img src="${img}" class="rounded-circle img-fluid col-1"  alt="user test alt">`;
+    let userDiv = `<div class="d-flex align-items-end mb-3">${avatar} <p class="mb-0"><b class="me-1">${post.author.name}</b>Says:</p></div>`;
+    return userDiv;
+  }
+  updatePost() {
+    console.log("late updated");
+  }
+  deletePost() {
+    console.log("late deleted");
   }
 }
